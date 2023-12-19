953aaa0e5f21b04df31b81b27012176c5d0001e9
Ian Wood <ianwood103@gmail.com>
2023-12-19 01:01:27 -0500

    projects section

--- src/data.ts
+++ src/data.ts
@@ -5,41 +5,48 @@ import webOne from "./assets/web-project-one.jpeg";
 import webTwo from "./assets/web-project-two.jpeg";
 import webThree from "./assets/web-project-three.jpeg";
 
-export const projects = [
-  // {
-  //   id: 0,
-  //   category: "uiUx",
-  //   img: uiUxOne,
-  //   title: "UI/UX Web Template",
-  // },
-  // {
-  //   id: 1,
-  //   category: "uiUx",
-  //   img: uiUxTwo,
-  //   title: "UI/UX Tablet Template",
-  // },
-  // {
-  //   id: 2,
-  //   category: "uiUx",
-  //   img: uiUxThree,
-  //   title: "Solar System App Design",
-  // },
-  // {
-  //   id: 3,
-  //   category: "web",
-  //   img: webOne,
-  //   title: "Interior Design Website",
-  // },
-  // {
-  //   id: 4,
-  //   category: "web",
-  //   img: webTwo,
-  //   title: "Agency Landing Page",
-  // },
-  // {
-  //   id: 5,
-  //   category: "web",
-  //   img: webThree,
-  //   title: "Crypto Landing Page",
-  // },
+interface Project {
+  id: number;
+  category: string;
+  img: string;
+  title: string;
+}
+
+export const projects: Project[] = [
+  {
+    id: 0,
+    category: "uiUx",
+    img: uiUxOne,
+    title: "UI/UX Web Template",
+  },
+  {
+    id: 1,
+    category: "uiUx",
+    img: uiUxTwo,
+    title: "UI/UX Tablet Template",
+  },
+  {
+    id: 2,
+    category: "uiUx",
+    img: uiUxThree,
+    title: "Solar System App Design",
+  },
+  {
+    id: 3,
+    category: "web",
+    img: webOne,
+    title: "Interior Design Website",
+  },
+  {
+    id: 4,
+    category: "web",
+    img: webTwo,
+    title: "Agency Landing Page",
+  },
+  {
+    id: 5,
+    category: "web",
+    img: webThree,
+    title: "Crypto Landing Page",
+  },
 ];
